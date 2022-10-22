baf474b4bd945f5b2213adc97de78e59a94e6eaf
gabrielcombali1 <gabrielcombalia@users.noreply.github.com>
2022-10-22 11:36:35 +0000

    bot receitas

--- main.rb
+++ main.rb
@@ -1,16 +1,38 @@
 receitas = []
-while (true) do
-  puts 'Digite a receita'
-  nome = gets.chomp()
-  receitas << nome
+puts
+puts "[1] Cadastrar uma receita"
+puts "[2] ver todas as receitas"
+puts "[3] sair"
 
+print "Escolha uma opção: "
+opcao = gets.chomp().to_i() #não precisa escrever o chomp. adicionei só para didática
+
+
+while (opcao != 3) do
+  if (opcao == 1)
+    puts
+    puts 'Digite a receita'
+    nome = gets.chomp()
+    receitas << nome
+    puts
+    puts "REceita #{nome} cadastrada"
+    puts
+  elsif (opcao == 2)
+    puts
+    puts '++++++++++++++++ Receitas Cadastradas ++++++++++++++++'
+    puts receitas
+    puts
+  else
+    puts "Opção inválida"
+  end
   puts
-  puts "REceita #{nome} cadastrada"
-  puts
-  puts '++++++++++++++++ Receitas Cadastradas ++++++++++++++++'
-  puts receitas
-  #receitas.each do |receita|
-  #  puts receita
-  #end
-  puts
+  puts "[1] Cadastrar uma receita"
+  puts "[2] ver todas as receitas"
+  puts "[3] sair"
+  
+  print "Escolha uma opção: "
+  opcao = gets.chomp().to_i() #não precisa escrever o chomp. adicionei só para didática
+
 end
+
+puts 'Obrigado. Tchau!'
