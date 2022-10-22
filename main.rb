2c59a500adfacc2bdc29f65abcc6a73a13298385
gabrielcombali1 <gabrielcombalia@users.noreply.github.com>
2022-10-22 17:23:04 +0000

    Métodos

--- main.rb
+++ main.rb
@@ -1,42 +1,47 @@
 receitas = []
-puts
-puts "[1] Cadastrar uma receita"
-puts "[2] ver todas as receitas"
-puts "[3] sair"
 
-print "Escolha uma opção: "
-opcao = gets.chomp().to_i() #não precisa escrever o chomp. adicionei só para didática
-
-
-while (opcao != 3) do
-  if (opcao == 1)
-    puts
-    puts 'Digite a receita'
-    nome = gets.chomp()
-    puts 'Digite o tipo da receita'
-    tipo = gets.chomp()
-    receita = { nome: nome, tipo: tipo }
-    receitas << receita
-    puts
-    puts "REceita #{receita[:nome]} cadastrada"
-    puts
-  elsif (opcao == 2)
-    puts
-    puts '++++++++++++++++ Receitas Cadastradas ++++++++++++++++'
-    receitas.each do |receita|
-      puts "#{receita[:nome]} - #{receita[:tipo]}"
-    end
-    puts
-  else
-    puts "Opção inválida"
-  end
+def menu()
   puts
   puts "[1] Cadastrar uma receita"
   puts "[2] ver todas as receitas"
   puts "[3] sair"
-  
   print "Escolha uma opção: "
-  opcao = gets.chomp().to_i() #não precisa escrever o chomp. adicionei só para didática
+  gets.to_i() #no ruby, a ultima linha será sempre cosiderada o return usado em outras linhas como o php
+end
+
+def inserir()
+  puts
+  puts 'Digite o nome da receita'
+  nome = gets.chomp()
+  puts 'Digite o tipo da receita'
+  tipo = gets.chomp()
+  receita = { nome: nome, tipo: tipo }
+    puts
+  puts "REceita #{receita[:nome]} cadastrada"
+  puts
+  receita #em ruby, não precisa do return
+end
+
+def imprime(r)
+  puts
+  puts '++++++++++++++++ Receitas Cadastradas ++++++++++++++++'
+  r.each do |receita|
+  puts "#{receita[:nome]} - #{receita[:tipo]}"
+  end
+  puts
+end
+
+opcao = menu()
+
+while (opcao != 3) do
+  if (opcao == 1)
+    receitas << inserir()
+  elsif (opcao == 2)
+    imprime(receitas)
+  else
+    puts "Opção inválida"
+  end
+  opcao = menu()
 
 end
 
