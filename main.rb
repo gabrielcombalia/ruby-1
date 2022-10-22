53d10ebb53a47014172580e37a37be899ac42aeb
gabrielcombali1 <gabrielcombalia@users.noreply.github.com>
2022-10-22 11:03:30 +0000

    com for natural

--- main.rb
+++ main.rb
@@ -3,5 +3,12 @@ while (true) do
   puts 'Digite a receita'
   nome = gets.chomp()
   receitas << nome
+
+  puts
   puts "REceita #{nome} cadastrada"
+  puts
+  puts '++++++++++++++++ Receitas Cadastradas ++++++++++++++++'
+  for receita  in receitas do
+    puts receita
+  end
 end
